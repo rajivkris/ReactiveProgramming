4b247914d9f2b852a3cb8b5192297b6d4a1f065e
Rajiv krishnan <rajivkris@gmail.com>
2022-03-22 17:03:53 +0530

    Kotlin Initializer

---
+++ functional-spring/src/test/kotlin/com/example/functionalspring/FunctionalSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.functionalspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FunctionalSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
